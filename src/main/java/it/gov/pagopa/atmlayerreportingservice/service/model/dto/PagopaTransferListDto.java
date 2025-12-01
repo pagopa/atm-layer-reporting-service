e62e35ec0920f11aa83ff8a2623a7ebf6619f234
Nicolas Pinto <nicolas.pinto@aurigaspa.com>
2025-12-01 16:59:01 +0100

    feat: add transactionId attribute in PagopaTransferListDto

--- src/main/java/it/gov/pagopa/atmlayerreportingservice/service/model/dto/PagopaTransferListDto.java
+++ src/main/java/it/gov/pagopa/atmlayerreportingservice/service/model/dto/PagopaTransferListDto.java
@@ -1,13 +1,17 @@
 package it.gov.pagopa.atmlayerreportingservice.service.model.dto;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public class PagopaTransferListDto {
     @NotNull
     public Long id;
+
+    @NotNull
+    public Long transactionId;
 
     @NotNull
     public Integer transferId;
