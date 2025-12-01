def473c6c38851f34a4cd89fedd7f91c4b3fab48
Nicolas Pinto <nicolas.pinto@aurigaspa.com>
2025-12-01 16:59:38 +0100

    feat: update PagopaTransferListMapper to map pagopaTransaction.id from transactionId

--- src/main/java/it/gov/pagopa/atmlayerreportingservice/service/model/mapper/PagopaTransferListMapper.java
+++ src/main/java/it/gov/pagopa/atmlayerreportingservice/service/model/mapper/PagopaTransferListMapper.java
@@ -1,19 +1,31 @@
 package it.gov.pagopa.atmlayerreportingservice.service.model.mapper;
 
-import it.gov.pagopa.atmlayerreportingservice.service.model.dto.PagopaTransferListDto;
-import it.gov.pagopa.atmlayerreportingservice.service.model.entity.PagopaTransferList;
 import java.util.List;
+
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.ObjectFactory;
+
+import it.gov.pagopa.atmlayerreportingservice.service.model.dto.PagopaTransferListDto;
+import it.gov.pagopa.atmlayerreportingservice.service.model.entity.PagopaTransactions;
+import it.gov.pagopa.atmlayerreportingservice.service.model.entity.PagopaTransferList;
 
 @Mapper(componentModel = "jakarta")
 public interface PagopaTransferListMapper {
-    @Mapping(target = "pagopaTransaction", ignore = true)
+
+    @Mapping(target = "pagopaTransaction.id", source = "transactionId")
     PagopaTransferList toEntity(PagopaTransferListDto dto);
 
+    @Mapping(target = "transactionId", source = "pagopaTransaction.id")
     PagopaTransferListDto toDto(PagopaTransferList entity);
 
     List<PagopaTransferListDto> toDtoList(List<PagopaTransferList> entities);
-
     List<PagopaTransferList> toEntityList(List<PagopaTransferListDto> dtos);
+
+    @ObjectFactory
+    default PagopaTransferList createEntity() {
+        PagopaTransferList entity = new PagopaTransferList();
+        entity.pagopaTransaction = new PagopaTransactions(); // necessario per popolazione nested
+        return entity;
+    }
 }
