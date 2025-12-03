ecaaf0f66bb853ef86d89d9d0980464cd66e3f39
Nicolas Pinto <nicolas.pinto@aurigaspa.com>
2025-12-03 15:18:23 +0100

    feat: add findByAbi method to CbillAbiFederazioneRepository for fetching by ABI

--- src/main/java/it/gov/pagopa/atmlayerreportingservice/service/model/repository/CbillAbiFederazioneRepository.java
+++ src/main/java/it/gov/pagopa/atmlayerreportingservice/service/model/repository/CbillAbiFederazioneRepository.java
@@ -3,7 +3,11 @@ package it.gov.pagopa.atmlayerreportingservice.service.model.repository;
 import io.quarkus.hibernate.reactive.panache.PanacheRepositoryBase;
 import it.gov.pagopa.atmlayerreportingservice.service.model.entity.CbillAbiFederazione;
 import jakarta.enterprise.context.ApplicationScoped;
+import io.smallrye.mutiny.Uni;
 
 @ApplicationScoped
 public class CbillAbiFederazioneRepository implements PanacheRepositoryBase<CbillAbiFederazione, String> {
+    public Uni<CbillAbiFederazione> findByAbi(String abi) {
+        return find("abi", abi).firstResult();
+    }
 }
