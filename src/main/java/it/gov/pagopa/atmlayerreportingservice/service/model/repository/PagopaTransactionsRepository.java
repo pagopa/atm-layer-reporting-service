4dc46ec158fee72ced68e5f578cc4a85a2d77834
Nicolas Pinto <nicolas.pinto@aurigaspa.com>
2025-12-03 14:31:25 +0100

    feat: add methods to PagopaTransferListRepository for fetching transfers by sender bank and transaction details

--- src/main/java/it/gov/pagopa/atmlayerreportingservice/service/model/repository/PagopaTransactionsRepository.java
+++ src/main/java/it/gov/pagopa/atmlayerreportingservice/service/model/repository/PagopaTransactionsRepository.java
@@ -3,7 +3,12 @@ package it.gov.pagopa.atmlayerreportingservice.service.model.repository;
 import io.quarkus.hibernate.reactive.panache.PanacheRepositoryBase;
 import it.gov.pagopa.atmlayerreportingservice.service.model.entity.PagopaTransactions;
 import jakarta.enterprise.context.ApplicationScoped;
+import io.smallrye.mutiny.Uni;
+import java.util.List;
 
 @ApplicationScoped
 public class PagopaTransactionsRepository implements PanacheRepositoryBase<PagopaTransactions, Long> {
+    public Uni<List<PagopaTransactions>> findBySenderBank(String senderBank) {
+        return find("senderBank", senderBank).list();
+    }
 }
--- src/main/java/it/gov/pagopa/atmlayerreportingservice/service/model/repository/PagopaTransferListRepository.java
+++ src/main/java/it/gov/pagopa/atmlayerreportingservice/service/model/repository/PagopaTransferListRepository.java
@@ -3,7 +3,20 @@ package it.gov.pagopa.atmlayerreportingservice.service.model.repository;
 import io.quarkus.hibernate.reactive.panache.PanacheRepositoryBase;
 import it.gov.pagopa.atmlayerreportingservice.service.model.entity.PagopaTransferList;
 import jakarta.enterprise.context.ApplicationScoped;
+import io.smallrye.mutiny.Uni;
+import java.util.List;
 
 @ApplicationScoped
 public class PagopaTransferListRepository implements PanacheRepositoryBase<PagopaTransferList, Long> {
+    public Uni<List<PagopaTransferList>> findBySenderBank(String senderBank) {
+        return find("pagopaTransaction.senderBank", senderBank).list();
+    }
+
+    public Uni<List<PagopaTransferList>> findUnreportedBySenderBank(String senderBank) {
+        return find("pagopaReported = false and pagopaTransaction.senderBank = ?1", senderBank).list();
+    }
+
+    public Uni<PagopaTransferList> findByTransactionIdAndTransferId(Long transactionId, Integer transferId) {
+        return find("transactionId = ?1 and transferId = ?2", transactionId, transferId).firstResult();
+    }
 }
