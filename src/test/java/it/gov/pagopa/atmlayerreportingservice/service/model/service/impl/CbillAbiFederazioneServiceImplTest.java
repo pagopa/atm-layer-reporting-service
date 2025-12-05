05da0c24dea96851384c0fd561ac0b1f5c634c6f
Nicolas Pinto <nicolas.pinto@aurigaspa.com>
2025-12-05 12:30:16 +0100

    test: update unit tests for CbillAbiFederazioneImplTest

--- src/test/java/it/gov/pagopa/atmlayerreportingservice/service/model/service/impl/CbillAbiFederazioneServiceImplTest.java
+++ src/test/java/it/gov/pagopa/atmlayerreportingservice/service/model/service/impl/CbillAbiFederazioneServiceImplTest.java
@@ -1,6 +1,5 @@
 package it.gov.pagopa.atmlayerreportingservice.service.model.service.impl;
 
-import io.quarkus.test.junit.QuarkusTest;
 import it.gov.pagopa.atmlayerreportingservice.service.model.entity.CbillAbiFederazione;
 import it.gov.pagopa.atmlayerreportingservice.service.model.repository.CbillAbiFederazioneRepository;
 import io.smallrye.mutiny.Uni;
@@ -9,57 +8,53 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-@QuarkusTest
 public class CbillAbiFederazioneServiceImplTest {
+
     @Test
-    void findAll_shouldReturnFederazioni_whenRepositoryReturnsData() {
+    void findAll_shouldReturnEntities() {
         CbillAbiFederazioneRepository repository = Mockito.mock(CbillAbiFederazioneRepository.class);
         CbillAbiFederazioneServiceImpl service = new CbillAbiFederazioneServiceImpl(repository);
-        CbillAbiFederazione entity = new CbillAbiFederazione();
-        Mockito.when(repository.listAll()).thenReturn(Uni.createFrom().item(List.of(entity)));
+        Mockito.when(repository.listAll()).thenReturn(Uni.createFrom().item(List.of(new CbillAbiFederazione())));
 
         UniAssertSubscriber<List<CbillAbiFederazione>> subscriber = service.findAll().subscribe().withSubscriber(UniAssertSubscriber.create());
 
-        subscriber.assertCompleted().assertItem(List.of(entity));
+        subscriber.assertCompleted();
         Mockito.verify(repository).listAll();
     }
 
     @Test
-    void findAll_shouldFail_whenRepositoryFails() {
-        CbillAbiFederazioneRepository repository = Mockito.mock(CbillAbiFederazioneRepository.class);
-        CbillAbiFederazioneServiceImpl service = new CbillAbiFederazioneServiceImpl(repository);
-        RuntimeException failure = new RuntimeException("error");
-        Mockito.when(repository.listAll()).thenReturn(Uni.createFrom().failure(failure));
-
-        UniAssertSubscriber<List<CbillAbiFederazione>> subscriber = service.findAll().subscribe().withSubscriber(UniAssertSubscriber.create());
-
-        subscriber.assertFailedWith(failure.getClass(), "error");
-        Mockito.verify(repository).listAll();
-    }
-
-    @Test
-    void findByAbi_shouldReturnEntity_whenRepositoryReturnsData() {
+    void findByAbi_shouldReturnEntity() {
         CbillAbiFederazioneRepository repository = Mockito.mock(CbillAbiFederazioneRepository.class);
         CbillAbiFederazioneServiceImpl service = new CbillAbiFederazioneServiceImpl(repository);
         CbillAbiFederazione entity = new CbillAbiFederazione();
-        Mockito.when(repository.findById("ABI")).thenReturn(Uni.createFrom().item(entity));
+        Mockito.when(repository.findById("12345")).thenReturn(Uni.createFrom().item(entity));
 
-        UniAssertSubscriber<CbillAbiFederazione> subscriber = service.findByAbi("ABI").subscribe().withSubscriber(UniAssertSubscriber.create());
+        UniAssertSubscriber<CbillAbiFederazione> subscriber = service.findByAbi("12345").subscribe().withSubscriber(UniAssertSubscriber.create());
 
         subscriber.assertCompleted().assertItem(entity);
-        Mockito.verify(repository).findById("ABI");
+        Mockito.verify(repository).findById("12345");
     }
 
     @Test
-    void findByAbi_shouldFail_whenRepositoryFails() {
+    void getPspConfiguration_shouldValidateAbi() {
         CbillAbiFederazioneRepository repository = Mockito.mock(CbillAbiFederazioneRepository.class);
         CbillAbiFederazioneServiceImpl service = new CbillAbiFederazioneServiceImpl(repository);
-        RuntimeException failure = new RuntimeException("error");
-        Mockito.when(repository.findById("ABI")).thenReturn(Uni.createFrom().failure(failure));
 
-        UniAssertSubscriber<CbillAbiFederazione> subscriber = service.findByAbi("ABI").subscribe().withSubscriber(UniAssertSubscriber.create());
+        UniAssertSubscriber<CbillAbiFederazione> subscriber = service.getPspConfiguration(null).subscribe().withSubscriber(UniAssertSubscriber.create());
 
-        subscriber.assertFailedWith(failure.getClass(), "error");
-        Mockito.verify(repository).findById("ABI");
+        subscriber.assertFailedWith(IllegalArgumentException.class, "ABI is required");
+    }
+
+    @Test
+    void getPspConfiguration_shouldReturnEntity() {
+        CbillAbiFederazioneRepository repository = Mockito.mock(CbillAbiFederazioneRepository.class);
+        CbillAbiFederazioneServiceImpl service = new CbillAbiFederazioneServiceImpl(repository);
+        CbillAbiFederazione entity = new CbillAbiFederazione();
+        Mockito.when(repository.findByAbi("54321")).thenReturn(Uni.createFrom().item(entity));
+
+        UniAssertSubscriber<CbillAbiFederazione> subscriber = service.getPspConfiguration("54321").subscribe().withSubscriber(UniAssertSubscriber.create());
+
+        subscriber.assertCompleted().assertItem(entity);
+        Mockito.verify(repository).findByAbi("54321");
     }
 }
